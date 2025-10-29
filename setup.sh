1ef7a6841b4a99b9c16c1da93172a6cebea41331
kozeliv-zxc <arkenvile@proton.me>
2025-10-30 01:22:15 +0700

    add setup file

---
+++ setup.sh
@@ -0,0 +1,15 @@
+user = $(shell whoami)
+apt = $(which apt)
+dnf = $(which dnf)
+
+if [ $user != "root" ]
+    echo "must be root"
+    exit 1
+
+if [ $apt ]; then
+    apt install git python3-dev python3-pip libffi-dev gcc libssl-dev python3-venv -y
+elif [ $dnf ]; then
+    dnf install git python3-devel libffi-devel gcc openssl-devel python3-libselinux -y
+else
+    echo "what the..."
+fi
